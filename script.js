45f331b29127c307af8cf0cfd26dca9899d62304
Jasur <jasurjamolovdev@gmail.com>
2023-01-09 12:58:45 +0500

    this is test message

---
+++ script.js
@@ -0,0 +1,7 @@
+const acces = prompt("Qaysi xizmatlardan foydalanishni xoxlaysiz?");
+if(acces) {
+    console.log("https://youtube.com/" + acces + "/videos");
+} else
+{
+    console.log("ups no work anything!");
+}
